20e487691b0fdbd0a0ddc06075eadb13d802c299
Chris Thomas <chris.thomas@antimatter-studios.com>
2017-12-09 01:21:12 +0100

    fix the insta-widget because there was a mistake in the jquery, it was using the return value of append()…which is node the correct node, but you can see how I fixed it, lets discuss it later
    Signed-off-by: Chris Thomas <chris.thomas@antimatter-studios.com>

--- index.php
+++ index.php
@@ -1,45 +1,47 @@
 <!DOCTYPE HTML>
 <html>
     <head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
-    <link rel="stylesheet" href="styles.css" />
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="js.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+        <link rel="stylesheet" href="styles.css" />
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.1/js/all.js"></script>
+        <script type="text/javascript" src="js.js"></script>
     </head>
 
     <body>
-        <div id="insta-widget" class="widget-large">
+        <div id="insta-widget" class="widget-small">
             <div class="heading">
-                <h1>Instagram</h1>
+                <p class="h1 section-header">Get the latest looks</p>
             </div>
             <div class="body clearfix">
                 <div class="loading">Loading...</div>
             </div>
             <div class="footing">
-                <h2><a href="#link" target="_blank">See More</a></h2>
+                <p class="h2 section-header">
+                    <a href="#link" target="_blank">See More</a>
+                </p>
                 <div class="user-info">
-                    <img src="#image" />
+                    <img src="#link" />
                     <span class="full-name"></span>
                     <span class="follower-count"></span> followers
                     <span class="post-count"></span> posts
                 </div>
             </div>
-            
+
             <template id="ig_image_template">
-                <div class="box col-md-3 col-sm-4 col-xs-6">
+                <div class="insta-item box grid-item medium--three-twelfths small--four-twelfths">
                     <div class="info">
                         <div class="likes">
-                            <span class="glyphicon glyphicon-heart"></span>
+                            <span class="fa fa-heart"></span>
                         </div>
                         <div class="comments">
-                            <span class="glyphicon glyphicon-comment"></span>
+                            <span class="fa fa-comment"></span>
                         </div>
-                        <!--div class="caption truncated"></div-->
                     </div>
                     <a class="info" href="#link" target="_blank"></a>
                     <img src="#image" />
                 </div>
             </template>
-        </div>
+        </div>  
     </body>
 </html>
--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     // https://stackoverflow.com/questions/9461621/how-to-format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900-in-javascrip
     function shortenLargeNumber(num) {
-        var units = ['k', 'M', 'G'],
-        decimal;
+        var units = ['k', 'M', 'G'];
+        var decimal;
 
         for(var i=units.length-1; i>=0; i--) {
             decimal = Math.pow(1000, i+1);
@@ -27,54 +27,53 @@ $(document).ready(function(){
             // 1. Find the template html element on the page
             var $ig_image_template = $('#ig_image_template');
 
-            // 2. Get the "content" prop'erty from the $template variable
+            // 2. Get the "content" prop'erty from the $ig_image_template variable
             var content = $ig_image_template.prop('content');
-            
-            // 3. Get the data 
-            var nodes = response.user.media.nodes.slice(0,12);
 
-            // 4. Find the widget html element on the page
+            // 3. Find the widget html element on the page
             var $insta_widget = $('#insta-widget');
             
-             // 5. Determine the display size of the widget and how many
-             //    boxes to display
-            var widget_size = $insta_widget.attr("class");
-             if(widget_size === "widget-small"){
-                nodes = nodes.slice(0,6);
-            } 
-            
-            // 6. Deletes the "Loading..." message
-            $insta_widget.find(".body").html("");
+            // 4. Determine whether the widget has "small" version enabled
+            var is_small = $insta_widget.hasClass("widget-small");
 
-            // 7. Process and display the data
+            // 5. Determine the display size of the widget and how many
+            //    boxes to display, either 6 or 12
+            var items = is_small ? 6 : 12;
+            
+            // 6. Get the data
+            var nodes = response.user.media.nodes.slice(0,items);
+
+            // 7. Deletes the "Loading..." message
+            $insta_widget.find(".body").html("");
+            
+            // 8. Process and display the data
             nodes.forEach(function(element){
-                // 7.1 Importing the templates content into the document as a new html element
-                var $clone = $(document.importNode(content,true));
+                // 8.1 Importing the templates content into the document as a new html element
+                $insta_widget.find(".body").append(document.importNode(content,true));
+                // 8.2 Display each image/display box in the widget
+                $clone = $insta_widget.find(".body").find(".insta-item:last-child");
                 
-                // 7.2 Adds the image into the display box
+                // 8.3 Adds the image into the display box
                 $clone.find("img").attr("src", element.thumbnail_src);
-                
-                // 7.3 Add the likes and comments onto the display box
+
+                // 8.4 Add the likes and comments onto the display box
                 $clone.find(".likes").append(shortenLargeNumber(element.likes.count));
                 $clone.find(".comments").append(shortenLargeNumber(element.comments.count));
-                
-                // 7.4 Add the caption onto the display box
-                // $clone.find(".caption").append(element.caption);
-                
-                // 7.5 Turn each display box into a link to the original instagram page
-                $clone.find("a").attr("href", "https://instagram.com/p/" + element.code );
 
-                // 7.6 Display each image/display box in the widget
-                $insta_widget.find(".body").append($clone);
+                // 8.5 Add the caption onto the display box
+                // $clone.find(".caption").append(element.caption);
+
+                // 8.6 Turn each display box into a link to the original instagram page
+                $clone.find("a").attr("href", "https://instagram.com/p/" + element.code );
             });
-            
-            // 8. Uses the display size of the widget to determine 
+
+            // 9. Uses the display size of the widget to determine 
             // the max image display size 
-            if(widget_size === "widget-small"){
-                $(".box").addClass("col-lg-2");
+            if(is_small){
+                $(".box").addClass("large--two-twelfths");
             }
 
-            // 9. Adds the footing at the bottom of the widget
+            // 10. Adds the footing at the bottom of the widget
             var instagram_url = "https://instagram.com/" + response.user.username;
             $insta_widget.find(".footing a").attr("href",instagram_url);
 
@@ -85,7 +84,7 @@ $(document).ready(function(){
             $user_info.find(".follower-count").text(shortenLargeNumber(user.followed_by.count));
             $user_info.find(".post-count").text(shortenLargeNumber(user.media.count));
 
-            // 10. Finishes loading all the data into the widget 
+            // 11. Finishes loading all the data into the widget 
             $insta_widget.addClass("loaded");
         } catch(e){
             console.log(e.message);
